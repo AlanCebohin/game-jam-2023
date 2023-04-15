d14294999214af97d2f61325ddd76cb4ed7f5662
AlanCebohin <alancebohin92@gmail.com>
2023-04-15 17:17:36 +0200

    Start BulletController

---
+++ Assets/Scripts/BulletController.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletController : MonoBehaviour
+{
+    private Rigidbody bulletRB;
+    [SerializeField] private float bulletSpeed;
+    private GameObject levelLimit;
+
+    //[SerializeField] private GameObject arrowImpact; // Particle effect
+    //[SerializeField] private EnemyController enemyController;
+    private Transform bulletTransform;
+
+    private void Awake()
+    {
+        bulletRB = GetComponent<Rigidbody>();
+        bulletTransform = GetComponent<Transform>();
+    }
+
+    private void Start()
+    {
+        bulletRB.velocity = bulletTransform.forward * bulletSpeed;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Enemy"))
+        {
+            //Instantiate(bulletImpact, bulletTransform.position, Quaternion.identity); // Particle effect
+            Destroy(collision.gameObject);
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnBecameInvisible()
+    {
+        Destroy(gameObject);
+    }
+}
